7eb1ab87be8287adfc9c6d3eff5c3ed9c67c4bee
sobhan <marktporg@gmail.com>
2025-03-30 14:43:05 +0330

    update login

--- login/login.js
+++ login/login.js
@@ -4,9 +4,11 @@ let lock = document.getElementById("lock");
 let onlock = document.getElementById("onlock");
 let a = document.querySelector("a");
 let passwordType = document.getElementById("ipt-password");
+let form = document.querySelector("form");
+let containerForm = document.querySelector(".container");
+let body = document.querySelector('body');
 
-
-onlock.style.display = "none"
+onlock.style.display = "none";
 
 lock.addEventListener("click",()=>{
     lock.style.display = "none";
@@ -17,4 +19,10 @@ lock.addEventListener("click",()=>{
         lock.style.display = "inline";
         passwordType.type = "password";
     })
+})
+
+
+form.addEventListener("click", ()=>{
+    containerForm.style.backgroundColor = "rgba(255, 255, 255, 0.256)";
+    containerForm.style.padding = "25px 10px"
 })
